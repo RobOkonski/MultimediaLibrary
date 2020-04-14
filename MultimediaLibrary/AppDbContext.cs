d4a2a85310f78b4f17eda67020e8feb2ac6503ba
Student 241540 <241540@student.pwr.edu.pl>
2020-04-14 14:27:41 +0200

    Repair DB context

--- MultimediaLibrary/AppDbContext.cs
+++ MultimediaLibrary/AppDbContext.cs
@@ -4,9 +4,6 @@ namespace MultimediaLibrary
 {
     class AppDbContext : DbContext
     {
-        /*public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }*/
         public DbSet<Artist> Artists { set; get; }
         public DbSet<Track> Tracks { set; get; }
 
@@ -15,7 +12,5 @@ namespace MultimediaLibrary
             optionsBuilder.UseSqlServer("Data Source=DESKTOP-0QE7KCN;Initial Catalog=MultimediaLibrary;Integrated Security=True");
             base.OnConfiguring(optionsBuilder);
         }
-
-
     }
 }
---
+++ MultimediaLibrary/Migrations/20200414122554_InitialCreate.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MultimediaLibrary.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Artists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    YoutubeAccountPath = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artists", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tracks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    ArtistId = table.Column<int>(nullable: false),
+                    YoutubePath = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tracks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tracks_Artists_ArtistId",
+                        column: x => x.ArtistId,
+                        principalTable: "Artists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tracks_ArtistId",
+                table: "Tracks",
+                column: "ArtistId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tracks");
+
+            migrationBuilder.DropTable(
+                name: "Artists");
+        }
+    }
+}
