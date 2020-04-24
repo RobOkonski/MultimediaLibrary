5bc7e6cfc0ec65cb9307f3796124a1830f6cbe6c
Student 241540 <241540@student.pwr.edu.pl>
2020-04-24 17:15:03 +0200

    Add AppDbContextFactory

---
+++ MultimediaLibrary/AppDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace MultimediaLibrary
+{
+    /// <summary>
+    /// AppDbContextFactory class
+    /// </summary>
+    /// <remarks>
+    /// Used with migrations
+    /// </remarks>
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        /// <summary>
+        /// Create AppDbContext for migrations
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns> Returns AppDbContext object </returns>
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MultimediaLibrary2;Trusted_Connection=True;");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
