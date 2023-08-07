26544a9327d5f4b0ebd76fd8135f5025fd952166
RobOkonski <51704485+RobOkonski@users.noreply.github.com>
2023-08-07 20:16:26 +0200

    Update AppDbContextFactory.cs

--- MultimediaLibrary/AppDbContextFactory.cs
+++ MultimediaLibrary/AppDbContextFactory.cs
@@ -20,7 +20,7 @@ namespace MultimediaLibrary
         public AppDbContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-            optionsBuilder.UseSqlServer(@"Server=tcp:psk-projekt.database.windows.net,1433;Initial Catalog=psk-projekt;Persist Security Info=False;User ID=robert;Password=#EDC4rfv;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            optionsBuilder.UseSqlServer(@"Server=tcp:psk-projekt.database.windows.net,1433;Initial Catalog=psk-projekt;Persist Security Info=False;User ID=robert;Password=<passwd>;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
             return new AppDbContext(optionsBuilder.Options);
         }
